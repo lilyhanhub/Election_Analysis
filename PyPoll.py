51d849b19d392f897afcb4330927e1031319fdca
lilyhanhub <106350763+lilyhanhub@users.noreply.github.com>
2022-07-03 22:55:10 -0700

    Update on PyPoll.py and the election_analysis.txt file

--- PyPoll.py
+++ PyPoll.py
@@ -85,11 +85,11 @@ with open(file_to_save, "w") as txt_file:
 
 # Print the winning candidates' results to the terminal
     winning_candidate_summary = (
-        f"----------------------------\n"
+        f"---------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Voter Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"----------------------------\n")
+        f"---------------------------\n")
     print(winning_candidate_summary)
 
 # Save the winning candidate's results to the text file
